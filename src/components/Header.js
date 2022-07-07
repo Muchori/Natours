c3bbbc2c1f503f8392b1d9c7a406029f468bf438
Muchori <muchorijoseph97@gmail.com>
2022-07-07 17:21:49 +0300

    SASS Configuration

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,19 @@ import React from 'react'
 function Header() {
   return (
     <header className='header'>
-      <div className='logo-box'>
+      <div className='header__logo-box'>
         <img
           src={require('../images/logo-white.png')}
           alt='Logo'
-          className='logo'
+          className='header__logo'
         />
       </div>
-      <div className='text-box'>
+      <div className='header__text-box'>
         <h1 className='heading-primary'>
-          <span className='heading-primary-main'>Outdoors</span>
-          <span className='heading-primary-sub'>is where life happens</span>
+          <span className='heading-primary--main'>Outdoors</span>
+          <span className='heading-primary--sub'>is where life happens</span>
         </h1>
-        <a href='#' className='btn btn-white btn-animated'>
+        <a href='#' className='btn btn--white btn--animated'>
           Discover our tours
         </a>
       </div>
